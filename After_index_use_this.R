f761902304f99efa0986e8254b130b80c2b690a3
Kevun Willis <xua9dg@virginia.edu>
2026-09-17 15:48:53 -0400

    Add files via upload

---
+++ After_index_use_this.R
@@ -0,0 +1,82 @@
+### install a new package; you only need to do this once.
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("Rsamtools",force=T)
+
+### libraries
+library(ggplot2)
+library(data.table)
+library(foreach)
+library(doMC)
+registerDoMC(2)
+library(Rsamtools)
+
+### this
+### specify the bam file
+fl <- system("ls -d /standard/BerglandTeach/data/Contamination_test/mapping_output/*/*.srt.flt.bam", intern=T)
+fl2 <- system("ls -d /standard/BerglandTeach/mapping_output/PRJNA185744/*.original.bam", intern=T)
+
+### How many reads map to each chromosome?
+rd <- foreach(bamFile=fl, .combine="rbind")%dopar%{
+  ## bamFile=fl2[1]
+  
+  ### tell me what file we are working on
+  message(bamFile)
+  
+  ### get the information about number of reads for each chromosome
+  if(!file.exists(paste(bamFile, ".bai", sep=""))) indexBam(bamFile)
+  stats <- as.data.table(idxstatsBam(bamFile))
+  
+  ### we need to subset to the main autosomal arms of melanogaster and simulans
+  setkey(stats, seqnames)
+  stats_small <- stats[J(c("2L", "2R", "3L", "3R", "X", "sim_2L", "sim_2R", "sim_3L", "sim_3R", "sim_X"))]
+  stats_small[grepl("sim", seqnames),species:="sim"]
+  stats_small[!grepl("sim", seqnames),species:="mel"]
+  
+  ### this command aggregates the data. For each unique value of species, we calculate the total number of mapped reads.
+  stats_small.ag <- stats_small[,list(mapped=sum(mapped), seqlength=sum(seqlength)), list(species)]
+  
+  ### format output
+  out <- data.table(propSim= stats_small.ag[species=="sim"]$mapped/sum( stats_small.ag$mapped))
+  
+  out[,nSim:=as.numeric(tstrsplit(bamFile, "\\.")[[8]])]
+  out[,nMel:=as.numeric(tstrsplit(bamFile, "\\.")[[10]])]
+  out[,samp:=last(tstrsplit(bamFile, "/"))]
+  
+  ### return output
+  return(out)
+}
+
+rd2 <- foreach(bamFile=fl2, .combine="rbind")%dopar%{
+  ## bamFile=fl2[1]
+  
+  ### tell me what file we are working on
+  message(bamFile)
+  
+  ### get the information about number of reads for each chromosome
+  if(!file.exists(paste(bamFile, ".bai", sep=""))) indexBam(bamFile)
+  stats <- as.data.table(idxstatsBam(bamFile))
+  
+  ### we need to subset to the main autosomal arms of melanogaster and simulans
+  setkey(stats, seqnames)
+  stats_small <- stats[J(c("2L", "2R", "3L", "3R", "X", "sim_2L", "sim_2R", "sim_3L", "sim_3R", "sim_X"))]
+  stats_small[grepl("sim", seqnames),species:="sim"]
+  stats_small[!grepl("sim", seqnames),species:="mel"]
+  
+  ### this command aggregates the data. For each unique value of species, we calculate the total number of mapped reads.
+  stats_small.ag <- stats_small[,list(mapped=sum(mapped), seqlength=sum(seqlength)), list(species)]
+  
+  ### format output
+  out <- data.table(propSim= stats_small.ag[species=="sim"]$mapped/sum( stats_small.ag$mapped))
+  
+  
+  out[,samp:=last(tstrsplit(bamFile, "/"))]
+  
+  ### return output
+  return(out)
+}
+
+### estimated vs real
+ggplot(data=rd, aes(x=realProp, y=propSim)) + geom_point() +
+  geom_hline(data=rd2, aes(yintercept=propSim, color=propSim))
